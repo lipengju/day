1bb96f0084474652e39957dab4730c995c5dc72c
lipengju <wangpingl@rayootech.com>
2015-10-14 14:03:41 +0800

    Primary Notepad

--- notepad.py
+++ notepad.py
@@ -15,10 +15,10 @@ def flag():
 
 def notepad():
 	try:
-		print '\t'*11,u'\033[32;5m欢迎使用记事本的程序\033[0m\t\t\t\t\n'
+		print '\t'*18,u'\033[32;5m欢迎使用记事本的程序\033[0m\n'
 		con=True
 		while con:
-			k=int(raw_input(u'请输入您的操作：\t\033[33;1m1.【我要写日记】\t2.【查看往事】\t3.【格式化笔记本】\t4.【备份文件】\t5.【恢复记事本】\t6.【搜索关键字】\t7.【退出】\033[0m\n'))
+			k=int(raw_input(u'请输入您的操作：\t\033[33;1m1.【我要写日记】\t2.【查看往事】\t3.【格式化笔记本】\t4.【备份文件】\t5.【恢复记事本】\t6.【搜索关键字】\t7【记事本分类】\t8【记事本文件列表】\t9.【退出】\033[0m\n'))
 			if k==1:
 				o=open(os.getcwd()+'notepad.log','a+')
 				content=raw_input(u'请输入您需要记录的事情:\n')
@@ -92,7 +92,33 @@ def notepad():
 							for i in search_Info_List:
 								print i
 							break_Flag=flag()
+
 			elif k==7:
+				k=raw_input(u'您确定要对记事本进行分类吗？(y/n):\n')
+				if k=='y':
+					print u'正在进行记事本分类...'
+					dirs=os.getcwd()+time.strftime('%Y-%m-%d',time.localtime())
+					os.makedirs(dirs)
+					files=dirs+'\\notepad.log'
+					f=open(files,'a+')
+					content=raw_input(u'请输入您需要记录的事情：\n')
+					f.writelines(content)
+					print u'记事本已分类完成！'
+				else:
+					continue
+
+			elif k==8:
+				def ListDir(path,fun,par):
+					for filespath in par:
+						print os.path.join(fun,filespath).strip(os.getcwd())
+				k=raw_input(u'是否要查看日记分类信息？(y/n):\n')
+				if k=='y':
+					print u'记事本分类信息为：\n'
+					os.path.walk(os.getcwd(),ListDir,())
+				else:
+					continue
+
+			elif k==9:
 				for i in range(3):
 					print u'\033[34;5m感谢您使用记事本程序，%s秒后程序将会自动退出！\033[0m'%(3-i)
 					time.sleep(1)
@@ -102,7 +128,7 @@ def notepad():
 				print u'\t\t\t\033[31;1m输入不合法，请重新输入，谢谢！\033[0m'
 				continue
 	except:
-		print u'输入错误!'
+		print u'输入错误，请再次执行程序！'
 
 if __name__=='__main__':
 	notepad()
